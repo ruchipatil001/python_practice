dc35467bad07d334706639f528b5d38195111dd4
ruchipatil001 <ruchicdac25@gmail.com>
2025-04-09 22:09:50 +0530

    Create eve_odd.py

---
+++ eve_odd.py
@@ -0,0 +1,7 @@
+Take a number from user. Print whether it is even number or odd number.
+---------------------------------------------------------------------------------------------------------------------------------------
+num=int(input("enter number "))
+if num%2==0:
+    print(num,"is even number")
+else:
+    print(num,"is odd number")
